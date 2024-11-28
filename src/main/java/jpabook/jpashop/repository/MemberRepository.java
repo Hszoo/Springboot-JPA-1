e657645076512435ad20d422ef92790f0c0765eb
Hszoo <trre1827151@gmail.com>
2024-11-28 22:22:04 +0900

    Section05. 회원 도메인 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,4 +1,48 @@
 package jpabook.jpashop.repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import jpabook.jpashop.domain.Member;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
 public class MemberRepository {
+
+    // @PersistenceContext : Spring에서 EntityManager를 만든 후 주입
+    private final EntityManager em;
+
+    // jpa를 이용한 객체 저장
+    public void save(Member member) {
+        // jpa가 member를 저장
+        em.persist(member);
+    }
+
+    // jpa를 이용한 객체 조회
+    public Member findOne(Long id) {
+        // jpa 제공 메서드로 id에 해당하는 객체를 찾아 return
+        return em.find(Member.class, id);
+    }
+
+    // jpa를 이용한 객체 list 조회 by. JPQL
+    public List<Member> findAll() {
+        // param1: JPQL, param2: 반환타입
+
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        // JPQL의 파라미터 바인딩 (by. setParameter())
+        return em.createQuery("select m from Member m where m.name=:name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+
+
+
 }
--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -1,4 +1,57 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // annot: 데이터 변경 로직 하나의 트랜잭션 내 수행
+// readOnly=True 조회 시 JPA가 성능 최적화 (이 서비스로직의 경우, 조회가 많아 클래스 레벨에서 readOnly로 설정)
 public class MemberService {
+
+    // @Autowired : Field Injection
+    private final MemberRepository memberRepository; // 생성자에서 주입된 이후에는 변경되지 않아, final로
+
+    // @Autowired : setter Injection
+    // public void setMemberRepository(MemberRepository memberRepository) {
+    //    this.memberRepository = memberRepository;
+    // }
+
+    // @Autowired : Constructor Injection (Springboot 최신버전에서는 생략 가능)
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+
+    /* 회원 가입 */
+    @Transactional // 쓰기는 별도로 Transcational annot 추가
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복 회원 검증 : 동시에 같은 name의 회원이 회원가입 -> 검증을 뚫을 수 있음 (방어: DB에서 unique제약을 추가)
+    private void validateDuplicateMember(Member member) {
+        // 중복 회원 : exception
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+
+        if(!findMembers.isEmpty()) // 같은 이름을 갖는 회원이 존재하는 경우
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+    }
+
+    /* 회원 전체 조회 */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /* 회원 1 조회 */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
 }
